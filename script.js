c22b4588e7d58c7289fcaebc8de9d6eb37b53358
Abeer08 <abeermanchanda00@gmail.com>
2022-05-05 12:14:45 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const div = document.getElementsByTagName('div');
+const inner_text = document.getElementById('inner-text');
+const celeb_gif = document.getElementById('celeb_gif');
+const h1 = document.getElementsByTagName('h1')[0];
+celeb_gif.style.display = 'none';
+
+var bday_song = new Audio('Tanmay.mp3');
+
+div[0].addEventListener('click', ()=>{
+    // Head or lid
+    div[1].style.top = '-220px';
+    div[1].style.left = '220px';
+    div[1].style.transform = 'rotateZ(60deg)';
+
+    // Dissappearing Shadow
+    div[4].style.backgroundColor = 'red';
+    div[5].style.backgroundColor = 'yellow';
+
+    // Heading 1
+    h1.style.display = 'none';
+
+    // Text
+    div[3].style.top = '-300px';
+    inner_text.style.fontSize = '2.5rem';
+
+    // if(bday_song.)
+    setTimeout(celebration, 1000);
+}, {once: true})
+
+function celebration(){
+    celeb_gif.style.display = 'block';
+    var bday_song = new Audio('Anu.mp3');
+    bday_song.play();
+}
